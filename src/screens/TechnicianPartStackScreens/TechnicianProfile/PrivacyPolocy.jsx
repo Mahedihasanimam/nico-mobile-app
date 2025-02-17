32db7ea49ecd3503ba7a13ef2eb256c6c2519142
Mahedihasanimam <mdmehedihasen678@gmail.com>
2025-02-17 11:08:22 +0600

    added: my account,editprofile,changepassword,

---
+++ src/screens/TechnicianPartStackScreens/TechnicianProfile/PrivacyPolocy.jsx
@@ -0,0 +1,12 @@
+import { View, Text } from 'react-native'
+import React from 'react'
+
+const PrivacyPolocy = () => {
+  return (
+    <View>
+      <Text>PrivacyPolocy</Text>
+    </View>
+  )
+}
+
+export default PrivacyPolocy
